5065943748102710d1808394e8b63bd28980e10b
serizao <serizao@users.noreply.github.com>
2015-11-03 09:37:03 +0100

    Update check_windows_network.sh

--- check_windows_network.sh
+++ check_windows_network.sh
@@ -10,9 +10,9 @@ host='IP_DE VOTRE_SERVER'
 mdp='MOT_DE_PASSE'
 # execution de check nt avec les paramètre ci dessus pour récupérer les octet envoyés et reçus le tout stocké dans les variable $result1 et $result2
 /usr/lib/nagios/plugins/check_nt -H $host -s $mdp -p $port -v COUNTER -l "$adaptateur1" -c $critical -w $warning > sertriskem.tmp
-read result1 < sertriskem.tmp
+read result1 < result.tmp
 /usr/lib/nagios/plugins/check_nt -H $host -s $mdp -p $port -v COUNTER -l "$adaptateur2" -c $critical -w $warning > sertriskem.tmp
-read result2 < sertriskem.tmp
+read result2 < result.tmp
 
 # verification par rapport aux varibles $warning et $critical
 if ([ $result1 -lt  $critical ] && [ $result1 -gt $warning ]) || ([ $result2 -lt  $critical ] && [ $result2 -gt $warning ]); then
